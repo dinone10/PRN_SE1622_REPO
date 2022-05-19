ccb1319e054524bdde3ee6234ddd8945371234fa
Admin <Admin@LAPTOP-T51LO5JK>
2022-05-19 15:01:29 +0700

    Add project files.

---
+++ PRN_SE1622_DEMO/Program.cs
@@ -0,0 +1,13 @@
+﻿Main();
+static void Main()
+{
+    int x = 1;
+    int y = 2;  
+    void ShowValue(int x)
+    {
+        Console.WriteLine($"x = {x}, a={a}");
+        x = 10;
+    }
+    ShowValue(x); 
+    Console.WriteLine(x);
+}
---
+++ PRN_SE1622_PRO/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace fptedu.se1622;
+public class Program
+{
+    public static void Main(String[] args)
+    {
+        Console.WriteLine($"Hello, World....");
+
+        Console.WriteLine($"Enter value x and y: ");
+        int x= int.Parse(Console.ReadLine());
+        int y= int.Parse(Console.ReadLine());   
+        int name;
+        var salary = 100.5;
+        
+        dynamic age = 20; //int32
+        age = true;//bool
+        Console.WriteLine($"Total of a ={x} and b={y} is {Sum(x, y)}");
+        Console.ReadLine();
+    }
+
+    public static int Sum(int a, int b)
+    {
+        return a + b;
+    }
+}
